68c43f8e54248ae0cbbb0924621ce35af1b02ba6
matiaswasiak <matiaswasiak@gmail.com>
2022-03-07 22:46:48 -0300

    add images

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,7 +4,9 @@ import styled from "styled-components";
 const Login = (props) => {
   return (
     <Container>
-      <Content>Content</Content>
+      <Content>
+        <BgImage />
+      </Content>
     </Container>
   );
 };
@@ -29,6 +31,20 @@ const Content = styled.div`
   flex-direction: column;
   padding: 80px 40px;
   height: 100%;
+`;
+
+const BgImage = styled.div`
+  height: 100%;
+  background-position: top;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-image: url("/images/login-background.jpg");
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+  right: 0;
+  z-index: -1;
 `;
 
 export default Login;
