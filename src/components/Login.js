cb268b54d2d1f111d181120226b409fd8d8580ae
matiaswasiak <matiaswasiak@gmail.com>
2022-03-07 23:33:58 -0300

    add logos two image

--- src/components/Login.js
+++ src/components/Login.js
@@ -13,6 +13,7 @@ const Login = (props) => {
             with a Disney+ subscription. As of 03/26/21, the price of Disney+
             and The Disney Bundle will increase by $1 .
           </Description>
+          <CTALogoTwo src="/images/cta-logo-two.png" alt="logo-two" />
         </CTA>
         <BgImage />
       </Content>
@@ -108,6 +109,14 @@ const Description = styled.p`
   margin: 0 0 24px;
   line-height: 1.5;
   letter-spacing: 1.5px;
+`;
+
+const CTALogoTwo = styled.img`
+  max-width: 650px;
+  margin-bottom: 20px;
+  display: inline-block;
+  vertical-align: bottom;
+  width: 100%;
 `;
 
 export default Login;
