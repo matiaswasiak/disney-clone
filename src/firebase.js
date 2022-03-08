5945a05769a7b6a095e73c630d2920bfb24cc0e7
matiaswasiak <matiaswasiak@gmail.com>
2022-03-08 01:53:03 -0300

    add sign in with google popup

---
+++ src/firebase.js
@@ -0,0 +1,22 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAswZk2oizA7rbkKSeBpbKr2Co_yVdsxw4",
+  authDomain: "disneyplus-clone-95434.firebaseapp.com",
+  projectId: "disneyplus-clone-95434",
+  storageBucket: "disneyplus-clone-95434.appspot.com",
+  messagingSenderId: "805167056173",
+  appId: "1:805167056173:web:ca2fc21687eacfff3cffa4",
+  measurementId: "G-WZYKFSFXHW",
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+// const storage = firebase.storage();
+
+export { auth, provider };
+export default db;
