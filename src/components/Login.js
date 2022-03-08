4f06e0530fc761625dc674ba59a8ffedb5f45c56
matiaswasiak <matiaswasiak@gmail.com>
2022-03-07 23:20:59 -0300

    add logos image

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,6 +5,9 @@ const Login = (props) => {
   return (
     <Container>
       <Content>
+        <CTA>
+          <CTALogoOne src="/images/cta-logo-one.svg" alt="" />
+        </CTA>
         <BgImage />
       </Content>
     </Container>
@@ -45,6 +48,35 @@ const BgImage = styled.div`
   left: 0;
   right: 0;
   z-index: -1;
+`;
+
+const CTA = styled.div`
+  /* margin-bottom: 2vw;
+  max-width: 650px;
+  flex-wrap: wrap;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin-top: 0;
+  text-align: center;
+  margin-right: auto;
+  margin-left: auto;
+  transition-timing-function: ease-out;
+  transition: opacity 0.2s;
+  width: 100%; */
+
+  max-width: 650px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
+const CTALogoOne = styled.img`
+  margin-bottom: 12px;
+  max-width: 600px;
+  min-height: 1px;
+  display: block;
+  width: 100%;
 `;
 
 export default Login;
