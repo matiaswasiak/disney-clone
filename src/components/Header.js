56d4ba7028a44d429130f3c0dc391b4ed7d7397b
matiaswasiak <matiaswasiak@gmail.com>
2022-03-09 00:07:57 -0300

    finish login functionality & redirect user

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
-import { useEffect } from "react";
 import styled from "styled-components";
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
 import { auth, provider } from "../firebase";
