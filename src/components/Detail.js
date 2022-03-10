5b948b79746145996f30c3640b248d6ff8bfff6f
matiaswasiak <matiaswasiak@gmail.com>
2022-03-10 01:48:05 -0300

    add detail background image

---
+++ src/components/Detail.js
@@ -0,0 +1,41 @@
+import styled from "styled-components";
+import React from "react";
+
+const Detail = (props) => {
+  return (
+    <Container>
+      <Background>
+        <img src="/images/viewers-disney.png" alt="" />
+      </Background>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  position: relative;
+  min-height: calc(100vh - 250px);
+  overflow-x: hidden;
+  display: block;
+  top: 72px;
+  padding: 0 calc(3.5vw + 5px);
+`;
+
+const Background = styled.div`
+  left: 0;
+  opacity: 0.8;
+  position: fixed;
+  right: 0;
+  top: 0;
+  z-index: -1;
+
+  img {
+    width: 100vw;
+    height: 100vh;
+
+    @media (max-width: 768px) {
+      width: initial;
+    }
+  }
+`;
+
+export default Detail;
