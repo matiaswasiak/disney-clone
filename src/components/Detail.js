a3d35eccaaf2c24cd4a2a38bf584fa84f1cec466
matiaswasiak <matiaswasiak@gmail.com>
2022-03-10 02:14:50 -0300

    add trailer

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -7,6 +7,21 @@ const Detail = (props) => {
       <Background>
         <img src="/images/viewers-disney.png" alt="" />
       </Background>
+      <ImageTitle>
+        <img src="/images/viewers-disney.png" alt="" />
+      </ImageTitle>
+      <ContentMeta>
+        <Controls>
+          <Player>
+            <img src="/images/play-icon-black.png" alt="" />
+            <span>Play</span>
+          </Player>
+          <Trailer>
+            <img src="/images/play-icon-white.png" alt="" />
+            <span>Trailer</span>
+          </Trailer>
+        </Controls>
+      </ContentMeta>
     </Container>
   );
 };
@@ -36,6 +51,79 @@ const Background = styled.div`
       width: initial;
     }
   }
+`;
+
+const ImageTitle = styled.div`
+  align-items: flex-end;
+  display: flex;
+  -webkit-box-pack: start;
+  justify-content: flex-start;
+  margin: 0 auto;
+  height: 30vw;
+  min-height: 170px;
+  padding: 24px;
+  width: 100%;
+
+  img {
+    max-width: 600px;
+    min-width: 200px;
+    width: 35vw;
+  }
+`;
+
+const ContentMeta = styled.div`
+  max-width: 874px;
+`;
+
+const Controls = styled.div`
+  align-items: center;
+  display: flex;
+  flex-flow: row nowrap;
+  margin: 24px 0;
+  min-height: 56px;
+`;
+
+const Player = styled.button`
+  font-size: 15px;
+  margin: 0 22px 0 0;
+  padding: 0 24px;
+  height: 56px;
+  border-radius: 4px;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  letter-spacing: 1.8px;
+  text-align: center;
+  text-transform: uppercase;
+  background: rgb(249, 249, 249);
+  border: none;
+  color: rgb(0, 0, 0);
+
+  img {
+    width: 32px;
+  }
+
+  &:hover {
+    background: rgb(198, 198, 198);
+  }
+
+  @media (max-width: 768px) {
+    height: 45px;
+    padding: 0 22px;
+    font-size: 12px;
+    margin: 0 10px 0 0;
+
+    img {
+      width: 25px;
+    }
+  }
+`;
+
+const Trailer = styled(Player)`
+  background: rgba(0, 0, 0, 0.3);
+  border: 1px solid rgb(249, 249, 249);
+  color: rgb(249, 249, 249);
 `;
 
 export default Detail;
