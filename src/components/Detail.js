e17218ab66383cc0b9c829997f29776f9e9402ea
matiaswasiak <matiaswasiak@gmail.com>
2022-03-10 02:30:01 -0300

    add subtitle and description

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -24,7 +24,14 @@ const Detail = (props) => {
             <span />
             <span />
           </AddList>
+          <GroupWatch>
+            <div>
+              <img src="/images/group-icon.png" alt="" />
+            </div>
+          </GroupWatch>
         </Controls>
+        <SubTitle>SubTitle</SubTitle>
+        <Description>Description</Description>
       </ContentMeta>
     </Container>
   );
@@ -157,6 +164,49 @@ const AddList = styled.div`
       transform: translateX(-8px) rotate(0deg);
       width: 2px;
     }
+  }
+`;
+
+const GroupWatch = styled.div`
+  height: 44px;
+  width: 44px;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  background: white;
+
+  div {
+    height: 40px;
+    width: 40px;
+    background-color: rgb(0, 0, 0);
+    border-radius: 50%;
+
+    img {
+      width: 100%;
+    }
+  }
+`;
+
+const SubTitle = styled.div`
+  color: rgb(249, 249, 249);
+  font-size: 15px;
+  min-height: 20px;
+
+  @media (max-width: 768px) {
+    font-size: 12px;
+  }
+`;
+
+const Description = styled.div`
+  line-height: 1.4;
+  font-size: 20px;
+  padding: 16px 0;
+  color: rgb(249, 249, 249);
+
+  @media (max-width: 768px) {
+    font-size: 14px;
   }
 `;
 
